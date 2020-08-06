2a31b9046fd96c45d0cc2b7d6518f9411215ba08
Norman Wong <nwnwnwnw4@gmail.com>
2020-08-06 10:59:16 -0700

    update notes graph method

--- graph_data.py
+++ graph_data.py
@@ -1,10 +1,11 @@
-# this script takes the result_data.txt file and creates an array of radar charts 
+# this script takes the result_data.txt file and creates an array of pie charts 
 
 import numpy
 import matplotlib.pyplot as plt
 import ast
 
 def result_data():
+    #reads the txt file and returns the data, ordered by total counts, in this format [(total count, key word, list of counts by color)]
     f = open('result_data.txt')
     result = ast.literal_eval(f.read())
     f.close()
@@ -24,18 +25,13 @@ colors = ['dimgrey', 'whitesmoke', 'mediumblue', 'black', 'tab:red','forestgreen
 for ax, (keyWordSum, KeyWordTitle, keyWordData) in zip(axes.flatten(), data):    
     KeyWordTitle += ' ({})'.format(int(keyWordSum))
     ax.set_title(KeyWordTitle, weight='bold', size='medium', position=(0.5, 1.1),
-                     horizontalalignment='center', verticalalignment='center')
-        
-    def actualValues(val):
-        value  = int(numpy.round(val/100.*keyWordSum, 0))
-        return value
-
+        horizontalalignment='center', verticalalignment='center')
     ax.pie(keyWordData, colors=colors, 
-    shadow=False, startangle=90, counterclock=False)
+        shadow=False, startangle=90, counterclock=False)
     ax.axis('equal')
 
 fig.text(0.5, 0.965, 'Most Frequently Used Keywords in Magic (Total Count Excluding Reprints)',
-             horizontalalignment='center', color='black', weight='bold',
-             size='large')
+    horizontalalignment='center', color='black', weight='bold',
+    size='large')
 
 plt.show()    
